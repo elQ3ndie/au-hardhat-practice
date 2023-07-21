ebaf7c7e2796fd7dc9e258556aa025594d74bcaf
Andrew Miracle <andrewmiracle300@gmail.com>
2023-07-21 20:48:46 +0100

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+const hre = require("hardhat");
+
+async function main() {
+
+
+  const counter = await hre.ethers.deployContract("Counter");
+
+  await counter.waitForDeployment();
+
+  console.log(
+    `Counter deployed to ${counter.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
